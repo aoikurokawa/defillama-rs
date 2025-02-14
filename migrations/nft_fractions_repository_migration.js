3c95ee4bc42cfa431a24bb6f732e6beb95f36cef
aoikurokawa <ht.so.ik0102@gmail.com>
2025-02-15 07:36:11 +0900

    feat: update

--- migrations/nft_fractions_repository_migration.js
+++
@@ -1,9 +0,0 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
-const MaticNftFractionsRepository = artifacts.require('MaticNftFractionsRepository');
-
-module.exports = async function (deployer, network) {
-    if (network === 'maticMumbai') {
-        await deployProxy(MaticNftFractionsRepository, ["URI"], { deployer });
-    }
-};
